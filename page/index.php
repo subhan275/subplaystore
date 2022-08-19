11b97dd86e4a0a45e26385a3928417cc1f645a26
subhan275 <subhanariq130@gmail.com>
2022-08-19 23:47:33 +0700

    font and card

--- page/index.php
+++ page/index.php
@@ -10,6 +10,7 @@ require "./function.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Subplay Store</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= $link ?>asset/css/style.css">
 </head>
 
 <body>
@@ -19,7 +20,7 @@ require "./function.php";
     <div class="container">
       <a class="navbar-brand" href="#">
         <img src="<?= $link ?>asset/mlbb_logo.jpg " alt="logo icon" width="50" height="50" class="me-2">
-        Subplay <strong>Store</strong>
+          <span class="font-subplay">Subplay <strong>Store</strong></span>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -73,6 +74,47 @@ require "./function.php";
     </div>
   </div>
   <!--  -->
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <h5>poppuler</h5>
+        <h2>Now Tranding</h2>
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="<?= $link ?>asset/mlbb_logo.jpg" class="card-img-top" alt="gambar1">
+          <div class="card-body">
+            <h5 class="card-title">MOBILE LEGEDS</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card" style="width: 18rem;">
+          <img src="<?= $link ?>asset/FF_logo.png" class="card-img-top" alt="gambar1">
+          <div class="card-body">
+            <h5 class="card-title">FREE FIRE</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+         <div class="card" style="width: 18rem;">
+          <img src="<?= $link ?>asset/mlbb_logo.jpg" class="card-img-top" alt="gambar1">
+          <div class="card-body">
+            <h5 class="card-title">PUBG MOBILE</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
   </script>
 </body>
